19cfcaeb1070d14cee5c7c67fab20b8147b9c1d7
zyq <313558158@qq.com>
2019-08-13 10:22:15 +0800

    Signed-off-by: zyq <313558158@qq.com>
    选择排序优化

--- sort/src/main/java/com/zyq/SelectionSort.java
+++ sort/src/main/java/com/zyq/SelectionSort.java
@@ -7,7 +7,7 @@ public class SelectionSort {
     public static void main(String[] args) {
         int[] arr = {5,9,6,5,56,1,0,33,999};
         System.out.println(Arrays.toString(arr));
-        selectionSort(arr);
+        selectionSort2(arr);
         System.out.println(Arrays.toString(arr));
     }
 
@@ -21,6 +21,26 @@ public class SelectionSort {
             int temp = arr[minIndex];
             arr[minIndex] = arr[i];
             arr[i] = temp;
+        }
+        return arr;
+    }
+
+    //选择排序优化,在一次循环中同时找出最大值和最小值
+    public static int[] selectionSort2(int[] arr){
+        for (int i=0;i<arr.length/2;i++){
+            int minIndex = i;
+            int maxIndex =arr.length -1 - i;
+            for (int j=i;j<arr.length - 1 - i;j++){
+                minIndex = arr[minIndex] < arr[j] ? minIndex : j ;
+                maxIndex = arr[maxIndex] > arr[j] ? maxIndex : j ;
+            }
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+
+            temp = arr[maxIndex];
+            arr[maxIndex] = arr[arr.length - 1 - i];
+            arr[arr.length - 1 - i] = temp;
         }
         return arr;
     }
