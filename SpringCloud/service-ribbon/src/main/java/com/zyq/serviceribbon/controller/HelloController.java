04a57f09394da3fdf7c5144040c1116849c5d333
zyq <313558158@qq.com>
2020-03-13 19:01:07 +0800

    Signed-off-by: zyq <313558158@qq.com>
    springcloud ribbon 启动失败

---
+++ SpringCloud/service-ribbon/src/main/java/com/zyq/serviceribbon/controller/HelloController.java
@@ -0,0 +1,24 @@
+package com.zyq.serviceribbon.controller;
+
+import com.zyq.serviceribbon.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *@author zyq
+ *@date 2020/3/13 18:27
+ *@description
+ **/
+@RestController
+public class HelloController {
+
+    @Autowired
+    HelloService helloService;
+
+    @RequestMapping(value = "/hi")
+    public String hi(@RequestParam String name){
+        return helloService.hiService(name);
+    }
+}
---
+++ SpringCloud/service-ribbon/src/main/java/com/zyq/serviceribbon/service/HelloService.java
@@ -0,0 +1,14 @@
+package com.zyq.serviceribbon.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.client.RestTemplate;
+
+public class HelloService {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    public String hiService(String name) {
+        return restTemplate.getForObject("http://SERVICE-HI/hi?name="+name,String.class);
+    }
+}
